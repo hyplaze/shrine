395d19d435bc6734619a0fd86861537f530c57cd
Chengyi Zhang <chriszhang09@g.ucla.edu>
2021-11-13 14:16:46 -0800

    establishes connection with mysql creates a prototype table for storing user information, currently successfully initiates a connection with mySql.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,41 @@
-const express = require('express');
-const BodyParser = require('body-parser');
+const express = require('express')
+const mysql = require('mysql')
 
-//const product = require('./routes/routes');
 const app = express();
 
-//app.use('/products', product);
+const db = mysql.createConnection({
+	host	:'localhost',
+	user	:'root',
+	password: 'shrine123456',
+	database: 'nodemysql'
+})
 
-app.get("/", (req, res) => {
-  res.json({ message: "xi wo de niu zi" });
+db.connect((err) =>{
+	if(err){
+		throw err;
+	}
+	console.log('mySql Connected ...')
+})
+
+app.get('/createdb', (req, res) => {
+	let sql = 'CREATE DATABASE IF NOT EXISTS nodemysql'
+	db.query(sql, (err, result) =>{
+		if(err) throw err;
+		console.log(result);
+		res.send('Database created ..')
+	})
 });
 
-let port = 1234;
+app.get('/createuserstable', (req, res) => {
+	let sql = 'CREATE TABLE users (id int AUTO_INCREMENT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY (id))';
+	db.query(sql, (err, result) => {
+		if (err)
+			throw err;	
+		console.log(result);
+		res.send('users table created...');
+	});
+});
 
-app.listen(port, ()=> {
-  console.log('Server is up on' + port);
+app.listen('3000', () =>{
+	console.log('Server started on port 3000');
 })
