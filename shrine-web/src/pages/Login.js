bc9462d53a11761faf9268b5b3ef7facd2dc458a
Alan Yu <25373334+AlanYu0712@users.noreply.github.com>
2021-11-28 02:08:30 -0800

    XMLHttprequest for Login Page
    Using axios, we are now able to retrieve the repsone from backend
    after posting our request to it.
    The axios would return the following 3 variable:
    response => all the response, including data, config, request, etc.
    
    resData => the data field of the response
    
    resStatus => Status field that returns whether we login successfully
    or not in the scope of the server side
    
    resCookie => the cookie returned from the server side for this login

--- shrine-web/src/pages/Login.js
+++ shrine-web/src/pages/Login.js
@@ -1,5 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 import { useState } from "react";
+
+const axios = require('axios'); //this is for axios intelisense
+
 
 function App() {
   const [email, setEmail] = useState("");
@@ -8,23 +11,35 @@ function App() {
   async function loginUser(event) {
     event.preventDefault();
 
-   
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
+    // console.log("Im here")
+    // const resData = axios({
+    //   method: 'post',
+    //   url: 'http://localhost:3000/login',
+    //   Email:"getBack@Beatles.edu",
+    //   mph: "2342313"
+    // })
+    // .then(res => { 
+    //   console.log(res);
+    //   return res.data;});
+
+    // console.log(resData)
+    const response = await axios({
+      method: 'post',
+      url: 'http://localhost:3000/login',
+      Email:"getBack@Beatles.edu",
+      mph: "2342313"
     });
 
-    const data = await response.json();
+    const resData = response.data; //all the response data
+    const resStatus = resData.Status; //response status
+    const resCookie = resData.cookie; //response cookie
 
-    if (data.user) {
-      localStorage.setItem("token", data.user);
-      alert("Login successful");
+    //check true,
+    //store user name,
+    //store cookie
+    if (resStatus) {
+      // localStorage.setItem("token", data.user); local storage, this is Hanry's stuff
+      console.log("Login successful");
       window.location.href = "/dashboard";
     } else {
       alert("Please check your username and password");
