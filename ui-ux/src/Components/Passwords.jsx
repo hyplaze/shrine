f5675c0d29833bfb111bb006e254b0a02795389c
hanryxu <54668181+hanryxu@users.noreply.github.com>
2021-11-29 14:40:26 -0800

    Fix display issue after changing Modal to react-bootstrap Modal

--- ui-ux/src/Components/Passwords.jsx
+++ ui-ux/src/Components/Passwords.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
+import { Modal, Button } from "react-bootstrap";
 import AddPasswordModal from "./AddPasswordModal.jsx"
-import InspectModal from "./InspectModal.jsx";
 
 export default
     class Passwords extends Component {
@@ -23,11 +23,75 @@ export default
                     notes: ""
                 }
             ],
-        }
+            entryInModal: {},
+            showModal: false,
+        };
+        this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.InspectModal = this.InspectModal.bind(this);
     }
+    handleClose() { this.setState({ showModal: false }); }
+    handleShow() { this.setState({ showModal: true }); }
 
+    InspectModal() {
+        return (
+            <>
+                <Button class="list-group-item list-group-item-action" aria-current="true" onClick={this.handleShow}>
+                    Inspect
+                </Button>
+
+
+                <Modal show={this.state.showModal} onHide={this.handleClose}>
+                    <Modal.Header>
+                        <Modal.Title>Add a new password</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Website Alias</h5>
+                                    <p class="card-text">pornhub</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">URL</h5>
+                                    <p class="card-text">www.pornhub.com</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Username</h5>
+                                    <p class="card-text">Eggert</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Password</h5>
+                                    <p class="card-text">12345</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Notes</h5>
+                                    <p class="card-text">123</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
     render = () => {
-
         return (
             <>
                 <div class="row" style={{ marginTop: "1%" }}>
@@ -45,9 +109,14 @@ export default
                             {this.state.entries.map((entry) => {
                                 return (
                                     <div class="row">
-                                        <div class="col-10">
+                                        <div class="col-6" style={{ paddingTop: "1%", paddingLeft: "2%" }}>
                                             <div class="list-group">
-                                                <InspectModal website={entry.website} url={entry.url} usr={entry.user} pwd={entry.pwd} notes={entry.notes}/>
+                                                {entry.website}<br />{entry.url}
+                                            </div>
+                                        </div>
+                                        <div class="col-2" style={{ paddingTop: "1%", paddingLeft: "2%" }}>
+                                            <div class="list-group">
+                                                <this.InspectModal />
                                             </div>
                                         </div>
                                         <div class="col-2" style={{ paddingTop: "1%", paddingLeft: "2%" }}>
