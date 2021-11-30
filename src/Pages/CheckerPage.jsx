4dbef3961c02eaddfdbaad1290666548d80aeec0
Alan Yu <25373334+AlanYu0712@users.noreply.github.com>
2021-11-29 20:25:49 -0800

    Implemented Feature for Check Password Breach
    Feature is able to traverse through all the user's boxes and
    determine which of the user's passwords have been breached.
    
    TODO: 	Need to add aesthetic to page.
    	(Be able to edit breached box)?

--- src/Pages/CheckerPage.jsx
+++ src/Pages/CheckerPage.jsx
@@ -7,65 +7,54 @@ const sha1 = require('js-sha1');
 var pwnd_dict = {}
 
 //TODO:
-//LOOP EVERY BOX,
-//for every BOX, do the following and return 
-//1. the number of breached boxes
-//2. All the boxname of breaches
-//3. How many times it has been compromised
-
+//get cookie to wherever it is stored [0]
+//make the output look nicer [1]
 async function getBreach(){
     const response = await axios({
         method: 'post',
-        url: 'http://localhost:3000/register',
+        url: 'http://localhost:3000/basicrequest',
         data:{
-            cookie: "adhfdfkajhwe" //later implemented
+            //[0] get cookie to wherever it is stroed
+            cookie:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtcGgiOiIxOTY1NzciLCJpYXQiOjE2MzgyNDIwNTksImV4cCI6MTYzODI1Mjg1OX0.S-f9kRaT7U7AIzTJYl8iSZlKfO00WMkn_5M71jaYCPc" //later implemented
         } 
-      }).data;
+      });
 
-      console.log(response)
-
-      response.forEach(checkBreach)
-
-
-
+      console.log(response.data)
+      for(const item of response.data){
+        await checkBreach(item);
+      }
 
 
+    async function checkBreach(item){
+        const digest = sha1(item.password).toUpperCase();
+        console.log("password is " + item.password);
 
-
-const digest = sha1('qwerty').toUpperCase();
-console.log(digest);
-
-const digestFront = digest.slice(0,5);
-const digestEnd = digest.slice(-35);
-console.log(digestFront);
-console.log(digestEnd);
-
-
-
-async function checkBreach(){
-    const res = await axios.get("https://api.pwnedpasswords.com/range/"+digestFront);
-    
-    let pwnd_list = res.data.split('\r\n');
-
-    
-
-    pwnd_list.forEach(createDictionary);
-
-    function createDictionary(item){
-        const pwnd_pair = item.split(":");
-        if(pwnd_pair[0]==digestEnd){
-            pwnd_dict[pwnd_pair[0]] = pwnd_pair[1];
-        }
+        const digestFront = digest.slice(0,5);
+        const digestEnd = digest.slice(-35);
+        console.log(digestFront);
+        console.log(digestEnd);
+        const res = await axios.get("https://api.pwnedpasswords.com/range/"+digestFront);
         
+        let pwnd_list = res.data.split('\r\n');
+
+        
+
+        pwnd_list.forEach(createDictionary);
+
+        function createDictionary(i){
+            const pwnd_pair = i.split(":");
+            if(pwnd_pair[0]==digestEnd){
+                pwnd_dict[item.boxname] = pwnd_pair[1];
+                console.log("I am here");
+            }
+            
+        }
+
     }
 
-}
+    const breaches = Object.keys(pwnd_dict).length;
+    console.log("breach are: " + breaches);
 
-const breaches = Object.keys(pwnd_dict).length;
-console.log("breach are: " + breaches);
-
-const response = getBreach();
-console.log("I am response " +response);
 }
 
 
@@ -74,30 +63,44 @@ class CheckerPage extends Component {
     constructor(props){
         super(props);
         this.state={
-            value: 0,
-            pressed:false
+            value: 0, //# of boxes we found has breached
+            pressed:false, //did we start to check yet?
+            breached:pwnd_dict //the list of boxnames and how many time it has been breached
         }
 
         this.Change = this.Change.bind(this);
     }
 
     Change(event){
-        getBreach();
-        this.setState({value:Object.keys(pwnd_dict).length, pressed:true});
-        console.log("my value is " + this.state.value);
+        getBreach().then(()=>{
+            this.setState(
+                {value:Object.keys(pwnd_dict).length, 
+                pressed:true,
+                breached:pwnd_dict});
+        })
+        
     }
 
 
     render() {
-        let p;
-        if(this.state.pressed){
+        let p; //text for showing if have any boxes that is breached, if there is display the number of it
+        let o=[]; //text for shwoing the boxname and how many times it is found.
+        let q; //text explain what you should do, what does this check mean
+        if(this.state.pressed){ //[1] make the output look nicer
             if(this.state.value!=0){
                 p=<p>{"WARNING: TOTAL "+this.state.value+" BREACHES"}</p>
+                for(var i=0; i<this.state.value; i++){
+                    o.push(
+                    <p key={"b"+i}>{"A password breached has been foun in the box \""+Object.keys(this.state.breached)[i]+"\""}</p>,
+                    <p key={"c"+i}>{"Its Password has been seen "+Object.values(this.state.breached)[i]+" times before"}</p>)
+                }
+                q=<i>{"Theses password has previously appeared in a data breach and should never be used. Please change password IMMEDIATELY!"}</i>
             }else{
                 p=<p>{"YOU ARE SAFE"}</p>
             }
         }
         
+        //[1] Make the output look nicer
         return (
             <div class="container-fluid">
 
@@ -106,9 +109,12 @@ class CheckerPage extends Component {
 
                     <button type="button card" class="list-group-item list-group-item-action" 
                     onClick={this.Change}>INSPECT</button>
-                   
                     {p}
+                    {o}
+                    {q}                    
                 </div>
+
+                <div class="row"><i>API provided by:<a href="https://haveibeenpwned.com/Passwords">';--have i been pwned?</a></i></div>
             </div>
         )
     }
